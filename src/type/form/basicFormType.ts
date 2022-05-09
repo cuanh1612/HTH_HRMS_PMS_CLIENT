bc4da05639d3732adcaccc153fd9ee2995fd9f2f
huy <huynguyencc1000@gmail.com>
2022-05-09 22:12:03 +0700

    Dev ui create event

--- src/type/form/basicFormType.ts
+++ src/type/form/basicFormType.ts
@@ -277,22 +277,6 @@ export interface createSignatureForm {
 	contract: number
 }
 
-<<<<<<< HEAD
-export interface ITimePicker {
-	required: boolean
-	name: string
-	placeholder: string
-	now?: boolean
-	label: string
-}
-
-export type AttendanceForm = {
-	clockIn: string
-	clockOut: string
-	late: boolean
-	halfDay: boolean
-	workingFrom: string
-=======
 export type createContractFileForm = {
 	files: {
 		name: string
@@ -300,7 +284,6 @@ export type createContractFileForm = {
 		url: string
 	}[]
 	contract: number
->>>>>>> dc5aea6828ba1cfe35f7815babc881a17c4f1772
 }
 
 export type createEventForm = {

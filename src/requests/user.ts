7b2dd6ecea0e1c2d3e66c03d4b270748915a76e3
nguyen quang hoang <hoanghpang@gmail.com>
2022-06-11 10:38:25 +0700

    update

--- src/requests/user.ts
+++ src/requests/user.ts
@@ -1,6 +1,7 @@
 import { userMutaionResponse } from 'type/mutationResponses'
 import { getData } from 'utils/fetchData'
 
+
 export const allUsersRequest = async (url: string) => {
 	return await getData<userMutaionResponse>({
 		url: `${process.env.NEXT_PUBLIC_API_URL}/api/${url}`,

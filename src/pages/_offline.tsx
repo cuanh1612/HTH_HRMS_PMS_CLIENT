c82fd6869aa0c5c08a0bbdc1d0dd60a6ac3a9d78
nguyen quang hoang <hoanghpang@gmail.com>
2022-07-01 11:06:18 +0700

    pwa website

--- src/pages/_offline.tsx
+++ src/pages/_offline.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { AuthContext } from 'contexts/AuthContext'
+import React, { useContext, useEffect } from 'react'
 
 export default function _offline() {
+    const {handleLoading} = useContext(AuthContext)
+    useEffect(() => {
+        handleLoading(false)
+    }, [])
   return (
     <div>_offline</div>
   )

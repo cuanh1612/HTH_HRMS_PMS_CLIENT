894b3d32b03f29eb03e033a555f29c24aafca17b
nguyen quang hoang <hoangdev161201@gmail.com>
2022-11-09 18:42:34 +0700

    upgrade

--- src/pages/api/auth/login-google.ts
+++ src/pages/api/auth/login-google.ts
@@ -68,6 +68,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 						resolve(true)
 					}
 				} catch (error) {
+					console.log(error)
 					;(res as NextApiResponse).status(500).json({
 						message: 'Something went wrong.',
 						code: 500,
